c9216c421de1bba0eb39cf1d5b489ade0489800a
Anshul Singh <iosdev747@users.noreply.github.com>
2018-01-27 01:47:11 +0530

    added basic gui

---
+++ VAR-sense/Assets/Scripts/ButtonAction.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ButtonAction : MonoBehaviour {
+
+	public void NextScene(string s)
+	{
+		SceneManager.LoadScene(s);
+	}
+}
