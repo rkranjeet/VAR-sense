8c42127ff1da24e6bee260f7fd185012c8e38ac9
Arsh Singh <33452790+iosdev474@users.noreply.github.com>
2018-01-27 16:18:48 +0530

    update controller

--- VAR-sense/Assets/Scripts/GameController.cs
+++ VAR-sense/Assets/Scripts/GameController.cs
@@ -1,1 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class GameController : MonoBehaviour {
+
+	[Range(0.00001f,3)]
+	float speed = 0.1f;
+
+	void Update () {
+		Vector3 velocity = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical")) * speed;
+		transform.Translate (velocity);
+		float rotation = 0;
+		if (Input.GetKey (KeyCode.Q)) {
+			rotation -= 5;
+		}
+		if (Input.GetKey (KeyCode.E)) {
+			rotation += 5;
+		}
+		transform.Rotate (0, rotation, 0);
+	}
+}
