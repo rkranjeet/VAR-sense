bcf2b1e3acdde79076c49b4fba24406d3ae61094
Anshul Singh <34132157+iosdev747@users.noreply.github.com>
2018-01-27 16:13:55 +0530

    Update ButtonAction.cs

--- VAR-sense/Assets/Scripts/ButtonAction.cs
+++ VAR-sense/Assets/Scripts/ButtonAction.cs
@@ -1,18 +1,1 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.SceneManagement;
-using UnityEngine;
 
-public class ButtonAction : MonoBehaviour {
-
-	public void NextScene(string s)
-	{
-		SceneManager.LoadScene(s);
-	}
-	public void quit(bool b)
-	{
-		if (b) {
-			Application.Quit ();
-		}
-	}
-}
